686bcd1a0aa439c07f345ebd726b445eadc2637b
Birgit Plakso <birgit.plakso@gmail.com>
2021-05-16 18:55:46 +0300

    16.05, test lisatud(ei tööta)

--- src/main/java/com/example/DecisionEngine/EngineController.java
+++ src/main/java/com/example/DecisionEngine/EngineController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RequestMapping("api")
 @RestController
 public class EngineController {
--- src/test/java/com/example/DecisionEngine/DecisionEngineApplicationTests.java
+++ src/test/java/com/example/DecisionEngine/DecisionEngineApplicationTests.java
@@ -6,22 +6,36 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class DecisionEngineApplicationTests {
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	void loanEngineWorksThroughAllLayers() throws Exception {
+    @Test
+    void loanEngineWorksThroughAllLayers() throws Exception {
 
-//		mockMvc.perform(get("/test/exception_test...")
-//				.contentType("application/json")
-//				.param("getLoanDecision", " "))
-//				.andExpect(status().isOk())
-//				.andExpect(content().object(LoanResponse.class));
+		mockMvc.perform(get("/loanDecision/5000/24/49002010998")
+				.contentType("application/json")
+				.param("amount", "5000")
+                .param("months","24")
+                .param("code","49002010998"))
+				.andExpect(status().isOk())
+				.andExpect(content().json("{\n" +
+                        "    \"enteredAmount\": 5000.0,\n" +
+                        "    \"enteredPeriod\": 24,\n" +
+                        "    \"proposedAmount\": 10000.0,\n" +
+                        "    \"proposedPeriod\": 24\n" +
+                        "}"));
 
 
-	}
+
+    }
+
 
 }
