8cd42f1b7f61bed19441173c68ef2e5155eb71ea
Birgit Plakso <birgit.plakso@gmail.com>
2021-05-20 15:10:13 +0300

    20.05. testi katsetused(ei tööta)

--- src/test/java/com/example/DecisionEngine/DecisionEngineApplicationTests.java
+++ src/test/java/com/example/DecisionEngine/DecisionEngineApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,19 +21,25 @@ class DecisionEngineApplicationTests {
     @Test
     void loanEngineWorksThroughAllLayers() throws Exception {
 
-		mockMvc.perform(get("/loanDecision/5000/24/49002010998")
-				.contentType("application/json")
-				.param("amount", "5000")
-                .param("months","24")
-                .param("code","49002010998"))
-				.andExpect(status().isOk())
-				.andExpect(content().json("{\n" +
-                        "    \"enteredAmount\": 5000.0,\n" +
-                        "    \"enteredPeriod\": 24,\n" +
-                        "    \"proposedAmount\": 10000.0,\n" +
-                        "    \"proposedPeriod\": 24\n" +
-                        "}"));
+        mockMvc.perform(get("/loanDecision/5000/24/49002010998") //? http://localhost:8080/api/...
+                .contentType("application/json")
+                .param("amount", "5000")
+                .param("months", "24")
+                .param("code", "49002010998"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.enteredAmount").value("5000.0"))
+                .andExpect(jsonPath("$.enteredPeriod").value("24"))
+                .andExpect(jsonPath("$.proposedAmount").value("10000.0"))
+                .andExpect(jsonPath("$.proposedPeriod").value("24"))
+                .andReturn();
 
+//and Expect oli esialgu nii:
+//                .andExpect(content().json("{\n" +
+//                        "    \"enteredAmount\": 5000.0,\n" +
+//                        "    \"enteredPeriod\": 24,\n" +
+//                        "    \"proposedAmount\": 10000.0,\n" +
+//                        "    \"proposedPeriod\": 24\n" +
+//                        "}"));
 
 
     }
